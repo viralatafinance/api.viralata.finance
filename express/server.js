66e87932a08d948469928491c7f45acfe4400401
- <->
2021-05-09 02:02:32 -0300

    cool

--- express/server.js
+++ express/server.js
@@ -10,10 +10,10 @@ const router = express.Router();
 
 require("dotenv").config();
 
-router.get("/", (req, res) => {
+router.get("*", (req, res) => {
   return res.json({
-    name: "Sleepy",
-    description: "Aww, looks like eating pancakes all day is tough work. Sweet dreams!",
+    name: "BITCAO",
+    description: req.url,
     image: "ipfs://QmYD9AtzyQPjSa9jfZcZq88gSaRssdhGmKqQifUDjGFfXm/sleepy.png",
     attributes: {
       bunnyId: "5",
@@ -24,16 +24,6 @@ router.get("/", (req, res) => {
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) =>
-  res.json({
-    name: "Sleepy",
-    description: "Aww, looks like eating pancakes all day is tough work. Sweet dreams!",
-    image: "ipfs://QmYD9AtzyQPjSa9jfZcZq88gSaRssdhGmKqQifUDjGFfXm/sleepy.png",
-    attributes: {
-      bunnyId: "5",
-    },
-  })
-);
 
 module.exports = app;
 module.exports.handler = serverless(app);
