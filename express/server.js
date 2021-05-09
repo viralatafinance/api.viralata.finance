bd3792d83babae9ecf09a1e1b3bf269a7eb03234
- <->
2021-05-09 01:15:31 -0300

    initial commit

---
+++ express/server.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const router = express.Router();
+
+require("dotenv").config();
+
+router.get("/", (req, res) => {
+  return res.json({
+    name: "Sleepy",
+    description: "Aww, looks like eating pancakes all day is tough work. Sweet dreams!",
+    image: "ipfs://QmYD9AtzyQPjSa9jfZcZq88gSaRssdhGmKqQifUDjGFfXm/sleepy.png",
+    attributes: {
+      bunnyId: "5",
+    },
+  });
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+
+module.exports = app;
+module.exports.handler = serverless(app);
